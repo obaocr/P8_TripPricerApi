0709ee40ac618bd1e1a5b6aec6d8f21187f4fcd3
BARBERIS Olivier <olivier.barberis@axa.fr>
2020-10-24 17:44:16 +0200

    init

---
+++ src/main/java/com/p8/tripPricer/tripPricerApi/TripPricerApiApplication.java
@@ -0,0 +1,13 @@
+package com.p8.tripPricer.tripPricerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPricerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPricerApiApplication.class, args);
+	}
+
+}
